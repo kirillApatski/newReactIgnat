c0bb2fad408ada0a99b344d2ac6a23ea08ae61ce
Kirill <apatski94@gmail.com>
2023-06-26 22:38:30 +0300

    HW 9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -34,8 +34,7 @@ function Clock() {
     }
 
     const stringTime = `${date.getHours().toString().length === 1 ? `0${date.getHours()}` : date.getHours()} : ${date.getMinutes().toString().length === 1 ? `0${date.getMinutes()}` : date.getMinutes()}: ${date.getSeconds().toString().length === 1 ? `0${date.getSeconds()}` : date.getSeconds()} `  || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.getDate() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-
+    const stringDate = date.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleString('default', {weekday: "long"}) || <br/> // пишут студенты
     const stringMonth = date.toLocaleString('dfault', {month: "long"}) || <br/> // пишут студенты
